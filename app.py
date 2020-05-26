f4b84ca971243c7b7fc8b6f3c49cf207d3e533a5
sharnoor404 <sharnoor404@gmail.com>
2020-05-26 20:05:34 +0530

    code

--- app.py
+++ app.py
@@ -30,3 +30,29 @@ data = load_data()
 st.sidebar.subheader("Show random tweet")
 random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
 st.sidebar.markdown(data.query("airline_sentiment == @random_tweet")[["text"]].sample(n=1).iat[0, 0])
+
+
+
+st.sidebar.markdown("### Number of tweets by sentiment")
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
+sentiment_count = data['airline_sentiment'].value_counts()
+sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("### Number of tweets by sentiment")
+    if select == 'Bar plot':
+        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
+
+st.sidebar.subheader("When and where are users tweeting from?")
+hour = st.sidebar.slider("Hour to look at", 0, 23)
+modified_data = data[data['tweet_created'].dt.hour == hour]
+if not st.sidebar.checkbox("Close", True, key='1'):
+    st.markdown("### Tweet locations based on time of day")
+    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
+    st.map(modified_data)
+    if st.sidebar.checkbox("Show raw data", False):
+        st.write(modified_data)
+
